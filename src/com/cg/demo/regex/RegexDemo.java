e5639bc419440e9d25eaf887a279ab29d404c767
vamandeshmukh <vamandeshmukh@gmail.com>
2022-05-25 10:14:24 +0530

    access modifiers

---
+++ src/com/cg/demo/regex/RegexDemo.java
@@ -0,0 +1,31 @@
+package com.cg.demo.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo {
+
+	public static void main(String[] args) {
+
+		String input = "asdwerabcde"; // swerarbcderr
+		String regex = "abc";
+
+		Pattern pat = Pattern.compile(regex);
+		Matcher mat = pat.matcher(input);
+
+		System.out.println(mat.matches()); // false
+		System.out.println(mat.lookingAt()); // true
+//		System.out.println(mat.find()); // true
+
+//		// character - a-z, A-Z, 0-9, @#$... 
+//		// email, password, mobile, ???
+//
+//		String email1 = "a@b.c";
+//		System.out.println(email1);
+//
+//		String email2 = "a#b*c";
+//		System.out.println(email2);
+
+	}
+
+}
