ffb59ddd44b4c6ef4577245d57f0505c00395ab0
vamandeshmukh <vamandeshmukh@gmail.com>
2022-05-28 10:55:51 +0530

    commit

--- src/com/cg/demo/ex/IllegalVotingException.java
+++ src/com/cg/demo/ex/IllegalVotingException.java
@@ -2,6 +2,7 @@ package com.cg.demo.ex;
 
 public class IllegalVotingException extends RuntimeException {
 
+	// ignore this field 
 	private static final long serialVersionUID = 1L;
 
 	public IllegalVotingException() {
